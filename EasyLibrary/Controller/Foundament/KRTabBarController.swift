2926c583c04ef6cd5b0f7bbdfdac87b983afb3a4
HuaKerong <engineerhkr@163.com>
2025-06-07 16:06:18 +0800

    import pod & some cutom file

---
+++ EasyLibrary/Controller/Foundament/KRTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  KRTabBarController.swift
+//  Practice5.27
+//
+//  Created by 华可容 on 2025/5/29.
+//
+
+import UIKit
+
+class KRTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
